9ef98fd9098bf1c990952028ed156cd566effe63
Noob gamer <legofanslv@gmail.com>
2020-07-22 17:08:38 +0300

    ver 17:08

---
+++ Browser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace ChromiumBasedBrowser
+{
+    public partial class Browser : Form
+    {
+        private ChromiumWebBrowser browser;
+
+        public Browser()
+        {
+            InitializeComponent();
+            InitializeBrowser();
+        }
+
+        private void InitializeBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser("https://datorium.eu");
+            browser.Height = 600;
+            browser.Width = 400;
+            browser.Dock = DockStyle.Fill;
+            this.Controls.Add(browser);
+        }
+    }
+}
